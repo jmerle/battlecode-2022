12afbd1bf5d75623930f93c56ea2228aeb39a171
Jasper van Merle <jaspervmerle@gmail.com>
2022-01-06 03:00:01 +0100

    v2

---
+++ src/camel_case_v2/dijkstra/Dijkstra.java
@@ -0,0 +1,9 @@
+package camel_case_v2.dijkstra;
+
+import battlecode.common.Direction;
+import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
+
+public interface Dijkstra {
+    Direction getBestDirection(MapLocation target) throws GameActionException;
+}

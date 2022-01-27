d936e8a0746a8a1ec57e96005f59ac7ecc3b047b
Jasper van Merle <jaspervmerle@gmail.com>
2022-01-28 00:23:04 +0100

    v25

---
+++ src/camel_case_v25/robot/droid/Droid.java
@@ -0,0 +1,18 @@
+package camel_case_v25.robot.droid;
+
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+import battlecode.common.RobotType;
+import camel_case_v25.dijkstra.Dijkstra;
+import camel_case_v25.robot.Robot;
+
+public abstract class Droid extends Robot {
+    public Droid(RobotController rc, RobotType type, Dijkstra dijkstra) {
+        super(rc, type, dijkstra);
+    }
+
+    @Override
+    public void run() throws GameActionException {
+        super.run();
+    }
+}
